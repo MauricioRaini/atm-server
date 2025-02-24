fd33acad4e8f3b1a7d4cebdedf3c862e54bd60ec
Mauricio Raini <mauricioraini777@gmail.com>
2025-02-24 07:22:02 -0600

    [ATM-1] feat(dependencies): dependencies injection singletons for auth

---
+++ src/config/dependencies.ts
@@ -0,0 +1,11 @@
+import { AuthController } from "../modules/auth/controllers/auth.controller";
+import { AuthService } from "../modules/auth/services/auth.service";
+import { AuthRepository } from "../modules/auth/repositories/auth.repository";
+import { JwtMiddleware } from "../modules/auth/middlewares/jwt.middleware";
+import { HashMiddleware } from "../modules/auth/middlewares/hash.middleware";
+
+const authRepository = new AuthRepository();
+const jwtMiddleware = new JwtMiddleware();
+const hashMiddleware = new HashMiddleware();
+const authService = new AuthService(authRepository, jwtMiddleware, hashMiddleware);
+export const authController = new AuthController(authService);
