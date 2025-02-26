03fc6fa0cf0af2deaae57d0db97f2da28170aa70
Mauricio Raini <mauricioraini777@gmail.com>
2025-02-25 20:44:23 -0600

    [ATM-2] feat(constants): error messages transactions

---
+++ src/modules/transactions/constants/messages.constants.ts
@@ -0,0 +1,6 @@
+export const TRANSACTION_ERROR_MESSAGES = {
+  INVALID_ACCOUNT: "Invalid Account",
+  INSUFFICIENT_FUNDS: "Insufficient funds",
+  DAILY_WITHDRAWAL_LIMIT_EXCEEDED: "Daily withdrawal limit exceeded",
+  DAILY_TRANSFER_LIMIT_EXCEEDED: "Daily transfer limit exceeded",
+};
