f458fe232dd092dad578a9c2cb2175deabb8b4fe
Mauricio Raini <mauricioraini777@gmail.com>
2025-02-26 00:04:19 -0600

    [ATM-2] feat(CORS): all routes temporary approach

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { transactionRoutes } from "./modules/transactions/routes";
+import cors from "cors";
 import { authRoutes } from "./modules/auth/routes";
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,8 @@ app.get("/health", (_req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
+/* TODO: customize CORS settings once deployment in the FE is done. */
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
